8b08d5fd53076ddf6bbc499b6af4d49b4e371077
efcinema <94225542+efcinema@users.noreply.github.com>
2021-12-27 12:26:10 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('🎬OTT UPDATES🎬', url=f'https://t.me/dvdnewupdates')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
